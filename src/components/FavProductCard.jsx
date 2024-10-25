2db58a62d3150edf4f2d06b652382c4430a7e4dc
Indra Puji Novirwan <indrapuji@gmail.com>
2024-10-25 16:30:24 +0700

    add icon and fix xcode build

--- src/components/FavProductCard.jsx
+++ src/components/FavProductCard.jsx
@@ -10,97 +10,74 @@ import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import formatRupiah from '@utils/formatRupiah';
 
-const FavProductCard = ({dataProduct}) => {
+const FavProductCard = ({item}) => {
   const navigation = useNavigation();
   const {width: SCREEN_WIDTH} = useWindowDimensions();
 
   const [checked, setChecked] = useState(false);
   return (
-    <View>
-      {dataProduct.map((item, index) => {
-        return (
-          <View key={index}>
-            <View
-              style={{
-                backgroundColor: 'white',
-                borderRadius: 16,
-                height: 150,
-                width: SCREEN_WIDTH - 36,
-
-                alignItems: 'center',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                onPress={() =>
-                  navigation.navigate('Detail', {
-                    data: item,
-                  })
-                }>
-                <Image
-                  source={item.image[0]}
-                  style={{
-                    height: 110,
-                    width: 110,
-                    borderRadius: 10,
-                    marginLeft: 16,
-                  }}
-                />
-              </TouchableOpacity>
-              <View
-                style={{
-                  marginLeft: 16,
-                  paddingRight: 120,
-                  height: 110,
-                  justifyContent: 'space-between',
-                }}>
-                <View>
-                  <TouchableOpacity
-                    onPress={() =>
-                      navigation.navigate('Detail', {
-                        data: item,
-                      })
-                    }>
-                    <Text
-                      style={[
-                        styles.PoppinsMedium,
-                        {width: SCREEN_WIDTH - 180},
-                      ]}
-                      numberOfLines={1}
-                      ellipsizeMode="tail">
-                      {item.title}
-                    </Text>
-                  </TouchableOpacity>
-                  <Text>{item.category}</Text>
-                  <Text>{formatRupiah(item.price)}</Text>
-                </View>
-                <TouchableOpacity
-                  onPress={() => setChecked(!checked)}
-                  style={{
-                    padding: 5,
-                    backgroundColor: checked ? 'yellow' : 'white',
-                    borderRadius: 50,
-                    alignItems: 'center',
-                    width: 120,
-                    borderWidth: 1,
-                  }}>
-                  <Text style={styles.PoppinsMedium}>Add to Cart</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-            {dataProduct.length - 1 !== index && (
-              <View
-                style={{
-                  backgroundColor: 'gray',
-                  height: 0.5,
-                  marginLeft: 16,
-                  marginRight: 16,
-                  marginVertical: 8,
-                }}
-              />
-            )}
-          </View>
-        );
-      })}
+    <View
+      style={{
+        backgroundColor: 'white',
+        height: 150,
+        width: SCREEN_WIDTH,
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 16,
+      }}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Detail', {
+            data: item,
+          })
+        }>
+        <Image
+          source={item.image[0]}
+          style={{
+            height: 110,
+            width: 110,
+            borderRadius: 10,
+            marginLeft: 16,
+          }}
+        />
+      </TouchableOpacity>
+      <View
+        style={{
+          marginLeft: 16,
+          paddingRight: 120,
+          height: 110,
+          justifyContent: 'space-between',
+        }}>
+        <View>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('Detail', {
+                data: item,
+              })
+            }>
+            <Text
+              style={[styles.PoppinsMedium, {width: SCREEN_WIDTH - 180}]}
+              numberOfLines={1}
+              ellipsizeMode="tail">
+              {item.title}
+            </Text>
+          </TouchableOpacity>
+          <Text>{item.category}</Text>
+          <Text>{formatRupiah(item.price)}</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => setChecked(!checked)}
+          style={{
+            padding: 5,
+            backgroundColor: checked ? 'yellow' : 'white',
+            borderRadius: 50,
+            alignItems: 'center',
+            width: 120,
+            borderWidth: 1,
+          }}>
+          <Text style={styles.PoppinsMedium}>Add to Cart</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/screens/FavoriteScreen.jsx
+++ src/screens/FavoriteScreen.jsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   useWindowDimensions,
   View,
+  FlatList,
 } from 'react-native';
 import React, {useState} from 'react';
 import productData from '@data/productData';
@@ -15,6 +16,21 @@ import FavProductCard from '@components/FavProductCard';
 
 const FavoriteScreen = ({}) => {
   const {width: SCREEN_WIDTH} = useWindowDimensions();
+
+  const renderHeader = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: 'white',
+          width: SCREEN_WIDTH,
+          marginTop: 10,
+          paddingBottom: 20,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+        }}
+      />
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -30,102 +46,14 @@ const FavoriteScreen = ({}) => {
         }}>
         <Text style={[styles.PoppinsBold, {fontSize: 20}]}>Wishlist</Text>
       </View>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          backgroundColor: 'white',
-          width: SCREEN_WIDTH,
-          marginTop: 10,
-          paddingBottom: 20,
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-        }}>
-        <FavProductCard dataProduct={productData} />
-        {/* {productData.map((item, index) => {
-          return (
-            <View key={index}>
-              <View
-                style={{
-                  backgroundColor: 'white',
-                  borderRadius: 16,
-                  height: 150,
-                  width: SCREEN_WIDTH - 36,
 
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                }}>
-                <TouchableOpacity
-                  onPress={() =>
-                    navigation.navigate('Detail', {
-                      data: item,
-                    })
-                  }>
-                  <Image
-                    source={item.image[0]}
-                    style={{
-                      height: 110,
-                      width: 110,
-                      borderRadius: 10,
-                      marginLeft: 16,
-                    }}
-                  />
-                </TouchableOpacity>
-                <View
-                  style={{
-                    marginLeft: 16,
-                    paddingRight: 120,
-                    height: 110,
-                    justifyContent: 'space-between',
-                  }}>
-                  <View>
-                    <TouchableOpacity
-                      onPress={() =>
-                        navigation.navigate('Detail', {
-                          data: item,
-                        })
-                      }>
-                      <Text
-                        style={[
-                          styles.PoppinsMedium,
-                          {width: SCREEN_WIDTH - 180},
-                        ]}
-                        numberOfLines={1}
-                        ellipsizeMode="tail">
-                        {item.title}
-                      </Text>
-                    </TouchableOpacity>
-                    <Text>{item.category}</Text>
-                    <Text>{formatRupiah(item.price)}</Text>
-                  </View>
-                  <TouchableOpacity
-                    onPress={() => setChecked(!checked)}
-                    style={{
-                      padding: 5,
-                      backgroundColor: checked ? 'yellow' : 'white',
-                      borderRadius: 50,
-                      alignItems: 'center',
-                      width: 120,
-                      borderWidth: 1,
-                    }}>
-                    <Text style={styles.PoppinsMedium}>Add to Cart</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-              {productData.length - 1 !== index && (
-                <View
-                  style={{
-                    backgroundColor: 'gray',
-                    height: 0.5,
-                    marginLeft: 16,
-                    marginRight: 16,
-                    marginVertical: 8,
-                  }}
-                />
-              )}
-            </View>
-          );
-        })} */}
-      </ScrollView>
+      <FlatList
+        data={productData}
+        keyExtractor={(key, i) => i.toString()}
+        renderItem={({item}) => <FavProductCard item={item} />}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={renderHeader}
+      />
     </View>
   );
 };
